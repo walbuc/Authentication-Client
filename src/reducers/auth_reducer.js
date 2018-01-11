d2df20b87dca3393edf6a330d5d5b51f486ae9c1
walter <buczacka.walter@gmail.com>
2018-01-11 16:41:28 -0200

    add reducers

---
+++ src/reducers/auth_reducer.js
@@ -0,0 +1,17 @@
+import { UNAUTH_USER, AUTH_USER, AUTH_ERROR, FETCH_MESSAGE } from '../actions/types'
+
+export default function(state = {}, action) {
+  switch(action.type) {
+    case AUTH_USER:
+      return { ...state, authenticated: true, error:'' }
+    case UNAUTH_USER:
+      return { ...state, authenticated: false }
+    case AUTH_ERROR:
+      return { ...state, error: action.payload }
+      //move this type to another reucer file. it is not part of auth.
+    case FETCH_MESSAGE:
+      return { ...state, message: action.payload }
+    default:
+      return state
+  }
+}
